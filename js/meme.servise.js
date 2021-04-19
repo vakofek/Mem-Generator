14d235485d741024886f32053f88c282c5de062a
ofek vaknin <vakofek10@gmail.com>
2021-04-19 14:17:11 +0300

    add editor

---
+++ js/meme.servise.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'add text',
+            size: 20,
+            aligb: 'left',
+            color: 'red'
+        }
+    ]
+};
